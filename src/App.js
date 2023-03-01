41bb6a26efb73317dec88b3c492f2c42fcea7b17
Mariam Hiza <mariamhhiza@egmail.com>
2023-03-01 21:43:09 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Create from "./Components/Create";
 import Read from "./Components/Read";
 import Update from "./Components/Update";
@@ -7,13 +7,13 @@ import Update from "./Components/Update";
 function App() {
   return (
     <div className="container">
-      <BrowserRouter basename="/react-dummy">
+      <HashRouter basename="/react-dummy">
         <Routes>
           <Route exact path="/" element={<Create />}></Route>
           <Route path="/read" element={<Read />}></Route>
           <Route path="/update" element={<Update />}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
